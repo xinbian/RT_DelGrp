2b1a0809206b70c6a929d9eab8193e27bed06781
Xin Bian <bianxin2006@gmail.com>
2017-09-15 16:43:11 -0400

    use loop to append step list

--- del_grp.py
+++ del_grp.py
@@ -1,3 +1,6 @@
+#this code delete some fields from hdf5 file
+#this can be used to delete "blow up results"
+
 import h5py
 import os
 import os.path
@@ -7,6 +10,7 @@ curDir = os.path.abspath(os.path.join(curfilePath,os.pardir))
 parentDir = os.path.abspath(os.path.join(curDir,os.pardir)) 
 
 inFile='tests_single_new.h5'
+specout=1000
 
 mylist = [parentDir,'/',inFile]
 delimiter = ''
@@ -15,9 +19,10 @@ filepath = delimiter.join(mylist)
 variable = ['PVz','PVy','PVx','PPress','Prho']
 H5File = h5py.File(filepath,'r+')
 step=['191000','192000','193000','194000','195000','196000','197000','198000','199000','200000','201000','202000','203000','204000','204272']
-#specout=1000
-#for i in range(651,718):
-#	step.append(str((i+1)*specout).zfill(6))
+
+for i in range(651,718):
+	step.append(str((i+1)*specout).zfill(6))
+
 for istep in step:
  for variable1 in variable:
 	delimiter = ''
